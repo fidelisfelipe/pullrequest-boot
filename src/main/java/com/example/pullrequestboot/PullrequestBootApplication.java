81cccdedd6ee56b7ec5b2a320806b96b17cc25d9
Fidelis Guimaraes <fidelis.guimaraes.ext@spms.min-saude.pt>
2024-02-18 17:14:37 +0000

    init project

---
+++ src/main/java/com/example/pullrequestboot/PullrequestBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.pullrequestboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PullrequestBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PullrequestBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pullrequestboot/PullrequestBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pullrequestboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PullrequestBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

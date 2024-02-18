9519b205c29a98f2cae9158ec176306c5b90b824
Fidelis Guimaraes <atosfiel@gmail.com>
2024-02-18 21:29:35 +0000

    fix(webhook): throws Exception

--- src/main/java/com/example/pullrequestboot/WebhookController.java
+++ src/main/java/com/example/pullrequestboot/WebhookController.java
@@ -4,13 +4,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+
 
 @RestController
 public class WebhookController {
 
     @PostMapping("/webhook/github")
-    public void handleWebhook(@RequestBody String payload) {
+    public void handleWebhook(@RequestBody String payload) throws UnsupportedEncodingException {
         // Lógica para lidar com a notificação do webhook
         String decodedPayload = URLDecoder.decode(payload, "UTF-8");
         System.out.println("Recebida notificação do webhook do GitHub: " + decodedPayload);

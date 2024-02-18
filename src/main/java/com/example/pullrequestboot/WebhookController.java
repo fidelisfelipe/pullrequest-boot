f4902cf2aae74ba9d62affa037939a0f5b666f69
Fidelis Guimaraes <fidelis.guimaraes.ext@spms.min-saude.pt>
2024-02-18 21:19:15 +0000

    feature(webhook): add encode utf-8 in payload

--- src/main/java/com/example/pullrequestboot/WebhookController.java
+++ src/main/java/com/example/pullrequestboot/WebhookController.java
@@ -4,13 +4,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URLDecoder;
+
 @RestController
 public class WebhookController {
 
     @PostMapping("/webhook/github")
     public void handleWebhook(@RequestBody String payload) {
         // Lógica para lidar com a notificação do webhook
-        System.out.println("Recebida notificação do webhook do GitHub: " + payload);
+        String decodedPayload = URLDecoder.decode(payload, "UTF-8");
+        System.out.println("Recebida notificação do webhook do GitHub: " + decodedPayload);
 
     }
 
